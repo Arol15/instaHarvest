bf9e5a77fae6498306cb0ae83c52cf411980c8de
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2021-01-25 18:56:11 -0500

    database setup

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask 
+from flask_migrate import Migrate
+
+
+from config import Config
+from app.models import db
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+migrate = Migrate(app, db)
 
-from app import routes 
+from app import routes
