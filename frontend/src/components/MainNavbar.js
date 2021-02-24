e1a95e7b66da45eca396717d64e8fb550168b71f
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2021-02-23 23:22:12 -0500

    navbar links to login and sign up

--- frontend/src/components/MainNavbar.js
+++ frontend/src/components/MainNavbar.js
@@ -1,10 +1,18 @@
+import { Link } from "react-router-dom"; 
+import "./MainNavbar.css"
 
 const MainNavbar = () => { 
+    
     return(
-        <nav>
+        <nav className='main-navbar'>
             <div>instaHarvest Logo</div>
-            <div>
-                login
+            <div classsName="main-navbar-links">
+                <div>
+                    <Link to="/login">Login</Link>
+                </div>
+                <div>
+                    <Link to="/signup">Sign Up</Link>
+                </div>
             </div>
         </nav>
     )
