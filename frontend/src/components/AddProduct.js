4a6ac6b8bef77504924c2285825c9f7ecfbebf67
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2021-02-26 16:42:27 -0500

    form to add product

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -1,12 +1,23 @@
 import useRequest from '../hooks/useRequest'; 
+import MainNavbar from './MainNavbar';  
 
 const AddProduct = () => {
 
     const [isLoading, data, error, errorNum, sendRequest] = useRequest(); 
 
+    
     return(
         <div>
-            add product
+            <MainNavbar />
+            <h2>Add your product</h2>
+            <form>
+                <label>Name of your product</label>
+                <input 
+                placeholder="Name" 
+                type="text" 
+                name="name" 
+                />
+            </form>
         </div>
     )
 }
