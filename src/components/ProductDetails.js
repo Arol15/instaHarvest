7327194118607685d9a118498d884d83eba7932b
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2021-03-09 14:15:55 -0500

    add auth when contacting seller

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,6 +1,10 @@
 import { useLocation, useHistory } from "react-router-dom"; 
 import { useEffect, useState } from "react"; 
 import useRequest from "../hooks/useRequest"; 
+import Spinner from './UI/Spinner'; 
+import { checkAuth } from "../utils/localStorage";
+import AuthModal from '../components/auth/AuthModal'; 
+import {useModal} from '../hooks/hooks'
 
 const ProductDetails = () => {
     const history = useHistory(); 
@@ -8,6 +12,12 @@ const ProductDetails = () => {
     const user_id = location.state.user_id; 
     const [isLoading, data, error, errorNum, sendRequest] = useRequest();
     const [info, setInfo] = useState({})
+
+    const [modal, showModal, closeModal] = useModal({
+        withBackdrop: true,
+        useTimer: false,
+        inPlace: false,
+      });
 
     const openChat = (recipientId, recipientName, recipientImg) => {
       history.push({
@@ -31,7 +41,9 @@ const ProductDetails = () => {
     }, [data])
 
     return (
+        <>
         <div>
+            {isLoading && <Spinner />}
             <div>
                 Product: {location.state.name}
             </div>
@@ -39,12 +51,18 @@ const ProductDetails = () => {
             <div>
                 Location for map: {info.lat} and {info.lgt}
             </div>
-            <button onClick={() => openChat(
-                location.state.user_id, 
-                info.first_name, 
-                info.image_url,
-            )}>Connect with seller</button>
+            {checkAuth() ? (  
+                <button onClick={() => openChat(
+                    location.state.user_id, 
+                    info.first_name, 
+                    info.image_url,
+                )}>Connect with seller</button>) : (
+                    <button onClick={() => showModal(<AuthModal />)}>Connect with Seller</button>
+                )
+            }
         </div>
+        {modal}
+        </>
 
     )
 }
