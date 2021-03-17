f259d0c132525c7e35ff7649cce1f89821c58a3a
Alexander Kovalyov <al.kovalyov@gmail.com>
2021-03-17 10:30:55 -0500

    Upd certificate

--- app/routes/cert.py
+++ app/routes/cert.py
@@ -3,9 +3,9 @@ from flask import Blueprint, make_response
 bp = Blueprint('cert', __name__, url_prefix='/')
 
 
-@bp.route("/.well-known/acme-challenge/DnzcRiG3xMGk7yHlf5LQWqENjkauKSgwJpJFOd9pqHs")
+@bp.route("/.well-known/acme-challenge/TSGse3htu8oLX4-hS4y0pwFUgMj5-6YaMaxs8ga91uk")
 def cert():
-    response = make_response("DnzcRiG3xMGk7yHlf5LQWqENjkauKSgwJpJFOd9pqHs.zjG3h0wgNQ - YKz7jFl51MrKMFMidtq5mE1fqLt3sfdc"
+    response = make_response("TSGse3htu8oLX4-hS4y0pwFUgMj5-6YaMaxs8ga91uk.zjG3h0wgNQ-YKz7jFl51MrKMFMidtq5mE1fqLt3sfdc"
                              )
     response.mimetype = "text/plain"
     return response
