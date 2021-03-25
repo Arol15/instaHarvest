6abd1443491fc947691aa608b80fa630b2aad4b8
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2021-03-24 23:13:48 -0400

    added style to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 //to use Mapbox 
 import {useState} from "react";
-import ReactMapGL, {Marker} from 'react-map-gl'; 
+import ReactMapGL, {Marker, Popup} from 'react-map-gl'; 
 
 
 const Map = () => {
@@ -11,11 +11,13 @@ const Map = () => {
         width: "100vw", 
         height: "100vh", 
         zoom: 8
-    })
+    });
+    
     return (
         <div>
             <ReactMapGL {...viewport}
             mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+            mapStyles="mapbox://styles/arol15/ckmoaqw8a43jc17qslhph1v2k"
             onViewportChange={viewport => {
                 setViewport(viewport)
             }}
